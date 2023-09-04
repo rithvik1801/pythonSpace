9369e769787fdb3921165c39ae8104d328addd7d
rithvik1801 <rithvik.munagala@gmail.com>
2023-09-04 14:47:04 -0400

    4thSep2023 Homework

--- difference between left side and right side.py
+++ difference between left side and right side.py
@@ -1,4 +1,5 @@
 #Write a program that inputs 2 * n integers and checks if the sum of the first n numbers (left sum) is equal to the sum of the second n numbers (right sum). If the sums are equal, print "Yes" + sum, if not, print "No" + difference. Calculate the difference as a positive number (absolute value).
+# for git checkout
 totalnum=int(input("How many numbers do you want to add? (Type only an even number.)"))
 lrun=1
 rrun=1
